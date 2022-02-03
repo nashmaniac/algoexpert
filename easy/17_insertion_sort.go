524b025ca31b41b424e2e950f30c51a7047afe05
Raju Ahmed Shetu <shetu2153@gmail.com>
2022-02-02 23:19:33 -0500

    palindrome checker added

--- easy/17_insertion_sort.go
+++ easy/17_insertion_sort.go
@@ -2,22 +2,10 @@ package easy
 
 func InsertionSort(array []int) []int {
 
-	placeToInsert := 0
-
-	for placeToInsert < len(array) {
-		currentMin := array[placeToInsert]
-		index := placeToInsert
-		minIndex := placeToInsert
-		for index < len(array) {
-			if currentMin > array[index] {
-				currentMin = array[index]
-				minIndex = index
-			}
-			index += 1
+	for i := 0;i<len(array); i++ {
+		for j := i;j>0 && array[j] < array[j-1]; j-- {
+			array[j], array[j-1] = array[j-1], array[j]
 		}
-
-		array[minIndex], array[placeToInsert] = array[placeToInsert], array[minIndex]
-		placeToInsert += 1
 	}
 	return array
 }
