715e2e220c2a850b345ed1363e7e36b8266fab3d
Raju Ahmed Shetu <shetu2153@gmail.com>
2022-02-07 00:56:35 -0500

    spiral traverse done

--- medium/3_spiral_traverse.go
+++ medium/3_spiral_traverse.go
@@ -1,44 +1,44 @@
 package medium
 
-import "log"
-
 func SpiralTraverse(array [][]int) []int {
 	if len(array) == 0 {
 		return nil
 	}
 
 	arr := make([]int, 0)
-	m := len(array)
-	n := len(array[0])
-	i, j := 0, 0
+	startRow := 0
+	endRow := len(array) - 1
+	startCol := 0
+	endCol := len(array[0]) - 1
 
-	for i < m/2 {
+	for startRow <= endRow && startCol <= endCol {
 		// traverse horizontal
-		for j < n {
-			arr = append(arr, array[i][j])
-			log.Println(arr)
-			j++
+		for i := startCol; i < endCol+1; i++ {
+			arr = append(arr, array[startRow][i])
 		}
-		j--
-		i++
-		for i < m {
-			arr = append(arr, array[i][j])
-			i++
+
+		for i := startRow + 1; i < endRow+1; i++ {
+			arr = append(arr, array[i][endCol])
 		}
-		i--
-		j--
-		for j >= 0 {
-			arr = append(arr, array[i][j])
-			j--
+
+		for i := endCol - 1; i >= startCol; i-- {
+			if startRow == endRow {
+				break
+			}
+			arr = append(arr, array[endRow][i])
 		}
-		j++
-		i--
-		for i > m {
-			arr = append(arr, array[i][j])
-			i--
+
+		for i := endRow - 1; i > startRow; i-- {
+			if startCol == endCol {
+				break
+			}
+			arr = append(arr, array[i][startCol])
 		}
-		m++
-		n--
+
+		startRow++
+		endRow--
+		startCol++
+		endCol--
 	}
 	return arr
 }
